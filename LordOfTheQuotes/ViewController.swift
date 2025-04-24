e9422900497e027763db745cd46bd7adc6021391
isTheo <matteohg07@gmail.com>
2025-04-24 19:21:06 +0200

    Initial commit

---
+++ LordOfTheQuotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LordOfTheQuotes
+//
+//  Created by Matteo Orru on 24/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
